3dfd4059fea73907f6bb40eb9e793e1904a95916
GaladrielLothlorien <galadriel1335@outlook.com>
2024-12-29 17:37:34 -0300

    Adicionando scripts

---
+++ hello.c
@@ -0,0 +1,12 @@
+//Biblioteca padrao de entrada e saida
+#include <stdio.h>
+
+//funcao main principal
+int main(void){
+
+    //Comando de saida 
+    printf("Ola, Mundo!\n");
+
+    //Retorna um inteiro da funcao main
+    return 0;
+}
